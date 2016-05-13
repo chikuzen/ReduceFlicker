e31f524dc05d859f64a70b43b201182778b9e852
OKA Motofumi <chikuzen.mo@gmail.com>
2016-05-13 20:06:00 +0900

    add license.

--- vapoursynth/src/proc_filter.h
+++ vapoursynth/src/proc_filter.h
@@ -1,3 +1,26 @@
+/*
+proc_filter.h: Copyright (C) 2016  Oka Motofumi
+
+Author: Oka Motofumi (chikuzen.mo at gmail dot com)
+
+This file is part of ReduceFlicker.
+
+This program is free software; you can redistribute it and/or
+modify it under the terms of the GNU Lesser General Public
+License as published by the Free Software Foundation; either
+version 2.1 of the License, or (at your option) any later version.
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+Lesser General Public License for more details.
+
+You should have received a copy of the GNU Lesser General Public
+License along with the author; if not, write to the Free Software
+Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA
+*/
+
+
 #ifndef REDUCE_FLICKER_PROC_FILTER_H
 #define REDUCE_FLICKER_PROC_FILTER_H
 
